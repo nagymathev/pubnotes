84be16d90d5ccce4f766e03ef956d45a5ee475e5
nagymathev <nagymathev@gmail.com>
2025-05-05 22:54:55 +0200

    Quartz sync: May 5, 2025, 10:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Squada One",
-        body: "Shantell Sans",
+        body: "Rubik",
         code: "IBM Plex Mono",
       },
       colors: {
